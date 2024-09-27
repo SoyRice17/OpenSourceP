775ef0e12b5d2e807821ace67d7732cafa8118a9
SoyRice17 <rkswkdqkq6974@gmail.com>
2024-09-27 12:24:15 +0900

    V0.3 Type

--- Go/week05/main.go
+++ Go/week05/main.go
@@ -1,10 +1,23 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+	"reflect"
+	"strings"
+)
 
 func main() {
-	var i int = 55
+	//var i int = 55
+
+	var f float32 = 4.30 // f := 4.30
+	i := 55
+
+	fmt.Println(reflect.TypeOf(f))
+	fmt.Printf("%s\n", strings.Title("hello world"))
+	fmt.Println(math.Ceil(3.99))
+
 	fmt.Printf("Value i: %d\n", i)
-	fmt.Println("Value i:", i, "\n")
+	fmt.Print("Value i: ", i, "\n")
 	fmt.Println("Value i:", i)
 }
