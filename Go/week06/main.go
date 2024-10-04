734dbf7315da5aa113a1b8d96ca7586e156e86c0
SoyRice17 <rkswkdqkq6974@gmail.com>
2024-10-04 10:35:31 +0900

    V0.4 Conversion

---
+++ Go/week06/main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+	"reflect"
+)
+
+func main() {
+	i := 13
+	f := 12.9
+
+	fmt.Printf("Value i: %d, Value f: %f\n", i, f)
+	//fmt.Printf("%d * %f = %f\n", i, f, i*f) invalid operation: i * f (mismatched types int and float64)
+	//fmt.Printf("%d * %f = %f\n", i, f, i*int(f))
+	fmt.Printf("%d * %f = %f\n", i, f, float64(i)*f)
+	fmt.Println(reflect.TypeOf(i))
+}
