b74f012942bead7ebf545e28b5d6b43b93d7eb3e
SoyRice17 <rkswkdqkq6974@gmail.com>
2024-10-04 10:40:40 +0900

    V0.5 Conversion

--- Go/week06/main.go
+++ Go/week06/main.go
@@ -7,11 +7,11 @@ import (
 
 func main() {
 	i := 13
-	f := 12.9
+	var f float64 = 12.9
 
 	fmt.Printf("Value i: %d, Value f: %f\n", i, f)
 	//fmt.Printf("%d * %f = %f\n", i, f, i*f) invalid operation: i * f (mismatched types int and float64)
-	//fmt.Printf("%d * %f = %f\n", i, f, i*int(f))
-	fmt.Printf("%d * %f = %f\n", i, f, float64(i)*f)
-	fmt.Println(reflect.TypeOf(i))
+	fmt.Printf("%d * %f = %d\n", i, f, i*int(f))
+	//fmt.Printf("%d * %f = %f\n", i, f, float64(i)*f)
+	fmt.Println(reflect.TypeOf(i), reflect.TypeOf(f))
 }
