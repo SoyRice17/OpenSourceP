49b281f583715ecfb68fcb44ea34111a9b74163e
SoyRice17 <rkswkdqkq6974@gmail.com>
2024-10-04 11:24:39 +0900

    V0.7 Zero Value

--- Go/week06/main.go
+++ Go/week06/main.go
@@ -2,16 +2,17 @@ package main
 
 import (
 	"fmt"
-	"reflect"
 )
 
 func main() {
-	i := 13
-	var f float64 = 12.9
+	var i int
+	var f float64
+	var b bool
+	var s string
 
-	fmt.Printf("Value i: %d, Value f: %f\n", i, f)
-	//fmt.Printf("%d * %f = %f\n", i, f, i*f) invalid operation: i * f (mismatched types int and float64)
-	fmt.Printf("%d * %f = %d\n", i, f, i*int(f))
-	//fmt.Printf("%d * %f = %f\n", i, f, float64(i)*f)
-	fmt.Println(reflect.TypeOf(i), reflect.TypeOf(f))
+	fmt.Println(f, b, s, i)
+	fmt.Printf("%f%t%s%d", f, b, s, i)
+	f = 2.7
+	i = 3
+	fmt.Print("\n\n", f > float64(i), "\n")
 }
