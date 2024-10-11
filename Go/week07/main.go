0c672d56d68775cb5ccd7090af589c3b710023b4
SoyRice17 <rkswkdqkq6974@gmail.com>
2024-10-11 10:45:52 +0900

    V1.1 Replace

--- Go/week07/main.go
+++ Go/week07/main.go
@@ -2,15 +2,14 @@ package main
 
 import (
 	"fmt"
-	"time"
+	"strings"
 )
 
 func main() {
-	var now time.Time = time.Now()
+	var foods string = "국?, 비빔?, 볶음?"
+	var replacer *strings.Replacer = strings.NewReplacer("?", "밥")
+	//= replacer := strings.NewReplacer("?", "밥")
 
-	//var month int = int(now.Month())
-
-	fmt.Printf("오늘은 %d년 %d월 %d일입니다. \n", now.Year(), now.Month(), now.Day())
-	fmt.Printf("지금은 %d시 %d분 %d초입니다. \n", now.Hour(), now.Minute(), now.Second())
-	fmt.Println(now.Month())
+	fmt.Println(foods)
+	fmt.Println(replacer.Replace(foods))
 }
