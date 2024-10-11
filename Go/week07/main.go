cd7b64fe474ba1705b36f8ec7583451b2452b93b
SoyRice17 <rkswkdqkq6974@gmail.com>
2024-10-11 11:26:18 +0900

    V1.3 Error

--- Go/week07/main.go
+++ Go/week07/main.go
@@ -3,14 +3,18 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"log"
 	"os"
 )
 
 func main() {
 	reader := bufio.NewReader(os.Stdin)
 	// = var reader *bufio.Reader = bufio.NewReader(os.Stdin)
-	fmt.Print("이름을 입력하세요: ")
+	fmt.Print("input your name: ")
 	name, err := reader.ReadString('\n')
-	fmt.Println(name)
-	fmt.Println(err)
+	if err != nil {
+		log.Fatal(err)
+	} else {
+		fmt.Println(name)
+	}
 }
