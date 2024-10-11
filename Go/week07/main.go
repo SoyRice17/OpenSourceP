4fc428f85812ac3748079a542c48ae1e9f95db69
SoyRice17 <rkswkdqkq6974@gmail.com>
2024-10-11 10:28:43 +0900

    V1.0 Time

--- Go/week07/main.go
+++ Go/week07/main.go
@@ -7,6 +7,10 @@ import (
 
 func main() {
 	var now time.Time = time.Now()
-	var month int = int(now.Month())
-	fmt.Println(month)
+
+	//var month int = int(now.Month())
+
+	fmt.Printf("오늘은 %d년 %d월 %d일입니다. \n", now.Year(), now.Month(), now.Day())
+	fmt.Printf("지금은 %d시 %d분 %d초입니다. \n", now.Hour(), now.Minute(), now.Second())
+	fmt.Println(now.Month())
 }
