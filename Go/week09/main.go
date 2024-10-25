6295df78e6dbd2064d1567e24eab6eb554a1ff03
SoyRice17 <rkswkdqkq6974@gmail.com>
2024-10-25 11:24:34 +0900

    V1.9 Guess Game Final

--- Go/week09/main.go
+++ Go/week09/main.go
@@ -16,6 +16,7 @@ func main() {
 	goalNumber := rand.Intn(6) + 1 // 1~3 사이의 랜덤한 수 생성
 	fmt.Printf("%d\n", goalNumber)
 
+	win := false
 	for i := 0; i < 3; i++ {
 		fmt.Printf("%d번의 기회가 있습니다. / 숫자 입력 : ", 3-i)
 		reader := bufio.NewReader(os.Stdin)
@@ -33,10 +34,17 @@ func main() {
 
 		if guessedNumber == goalNumber {
 			fmt.Println("정답입니다.")
+			win = true
+			break
 		} else if guessedNumber > goalNumber {
 			fmt.Println("입력하신 수는 정답보다 큰 수 입니다.")
 		} else {
 			fmt.Println("입력하신 수는 정답보다 작은 수 입니다.")
 		}
+	}
+	if win {
+		fmt.Println("승리하였습니다.")
+	} else {
+		fmt.Println("실패하였습니다.")
 	}
 }
