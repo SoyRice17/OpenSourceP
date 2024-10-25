e26c7ca3f9cbfb1e4c34c1203686752c98c91873
SoyRice17 <rkswkdqkq6974@gmail.com>
2024-10-25 11:12:03 +0900

    V1.8 Guess Game

--- Go/week09/main.go
+++ Go/week09/main.go
@@ -13,28 +13,30 @@ import (
 
 func main() {
 	rand.Seed(time.Now().Unix())
-	goalNumber := rand.Intn(3) + 1 // 1~3 사이의 랜덤한 수 생성
+	goalNumber := rand.Intn(6) + 1 // 1~3 사이의 랜덤한 수 생성
 	fmt.Printf("%d\n", goalNumber)
 
-	fmt.Print("숫자 입력 : ")
-	reader := bufio.NewReader(os.Stdin)
-	input, err := reader.ReadString('\n')
-	if err != nil {
-		log.Fatal(err)
-	}
+	for i := 0; i < 3; i++ {
+		fmt.Printf("%d번의 기회가 있습니다. / 숫자 입력 : ", 3-i)
+		reader := bufio.NewReader(os.Stdin)
+		input, err := reader.ReadString('\n')
+		if err != nil {
+			log.Fatal(err)
+		}
 
-	input = strings.TrimSpace(input)
-	guessedNumber, err := strconv.Atoi(input)
-	if err != nil {
-		log.Fatal(err)
-	}
-	fmt.Println(guessedNumber)
+		input = strings.TrimSpace(input)
+		guessedNumber, err := strconv.Atoi(input)
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Println(guessedNumber)
 
-	if guessedNumber == goalNumber {
-		fmt.Println("정답입니다.")
-	} else if guessedNumber > goalNumber {
-		fmt.Println("입력하신 수는 정답보다 큰 수 입니다.")
-	} else {
-		fmt.Println("입력하신 수는 정답보다 작은 수 입니다.")
+		if guessedNumber == goalNumber {
+			fmt.Println("정답입니다.")
+		} else if guessedNumber > goalNumber {
+			fmt.Println("입력하신 수는 정답보다 큰 수 입니다.")
+		} else {
+			fmt.Println("입력하신 수는 정답보다 작은 수 입니다.")
+		}
 	}
 }
