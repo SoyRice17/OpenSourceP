f1df8591c5c407c201b2f11eac6363ed760e4bc7
SoyRice17 <rkswkdqkq6974@gmail.com>
2024-11-15 11:45:07 +0900

    V4.0 Array

--- Go/week12/main.go
+++ Go/week12/main.go
@@ -2,25 +2,19 @@ package main
 
 import (
 	"fmt"
-	"time"
+
+	"github.com/headfirstgo/keyboard"
 )
 
 func main() {
-	// var dates [3]time.Time
-	// dates[1] = time.Unix(1457894000, 0)
-	// fmt.Println(dates[1])
+	var gpa [3]float64
 
-	dates := [3]time.Time{
-		time.Unix(1257894000, 0),
-		time.Unix(1457894000, 0),
-		time.Unix(1657894000, 0),
+	//go get github.com/headfirstgo/keyboard
+	for i := 0; i < len(gpa); i++ {
+		fmt.Print("Input float number : ")
+		gpa[i], _ = keyboard.GetFloat()
 	}
-	// fmt.Println(dates[0], dates[1], dates[2]) // 배열 원소들
-	// fmt.Println(dates)                       // 배열 자체
-	// fmt.Printf("%#v\n", dates) // 배열 리터럴 (리터럴이란? 소스코드에 직접 표현되는 값)
-
-	//for i := 0; i < len(dates); i++ { // 안전함ss
-	for _, v := range dates { // 파이썬 enumerate와 유사함 , _는 인덱스를 무시하겠다는 의미
-		fmt.Println(v)
+	for i, v := range gpa {
+		fmt.Printf("GPA %d: %f\n", i, v)
 	}
 }
