1c0aa09e45243c8f3ff50b9e5b91fd413e72bdd7
SoyRice17 <rkswkdqkq6974@gmail.com>
2024-11-29 11:08:38 +0900

    V4.7 Map

--- Go/week14/main.go
+++ Go/week14/main.go
@@ -1,46 +1,24 @@
 package main
 
-import (
-	"fmt"
-	"log"
-
-	"github.com/headfirstgo/datafile" // go get github.com/headfirstgo/datafile
-)
+import "fmt"
 
 func main() {
-	lines, err := datafile.GetStrings("votes.txt")
-	if err != nil {
-		log.Fatal(err)
-	}
-	var names []string
-	var counts []int
-	for _, line := range lines {
-		matched := false
-		for i, name := range names {
-			if name == line {
-				counts[i]++
-				matched = true
-			}
-		}
-		if !matched { // 중복된 이름이 없으면
-			names = append(names, line) // 이름 배열에 추가
-			counts = append(counts, 1)  // 카운트 배열에 1 추가
-		}
-	}
+	ages := make(map[string]int)
 
-	for i, name := range names {
-		fmt.Printf("Votes for %s: %d\n", name, counts[i])
-	}
+	var name string
+	var age int
 
-	//lines, err := datafile.GetStrings("votes.txt")
-	//if err != nil {
-	//	log.Fatal(err)
-	//}
-	//counts := make(map[string]int)
-	//for _, line := range lines {
-	//	counts[line]++
-	//}
-	//for name, count := range counts {
-	//	fmt.Printf("Votes for %s: %d\n", name, count)
-	//}
+	for {
+		fmt.Print("Enter name: (exit to 'q')")
+		fmt.Scanln(&name)
+		if name == "q" {
+			break
+		}
+		fmt.Print("Enter age: ")
+		fmt.Scanln(&age)
+		ages[name] = age
+	}
+	for name, age := range ages {
+		fmt.Printf("%s\t%d\n", name, age)
+	}
 }
